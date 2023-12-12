7c3f465e64894ca71aed7dc15c01481421f58f1b
public satanic void <55899222+PublicSatanicVoid@users.noreply.github.com>
2023-12-11 20:18:18 -0600

    Update psall.sh
    more compact output, swap state and pcpu cols

--- psall.sh
+++ psall.sh
@@ -4,20 +4,14 @@
 # ps prints etime in the format "days-hours:minutes:seconds" when a process has been running for
 # more than a day. But this takes up more space than just showing "hours:minutes:seconds" so this
 # script converts to that format. (There's no way to specify this in ps itself)
-# Also this uses the minimal padding between columns needed (plus one extra space for readability,
-# between %cpu and etime, since they are adjacent numerical columns)
+# Also this uses the minimal padding between columns needed
 
-ps f -o pid,user,state,%cpu,etime,args -N --ppid 2 | awk '
-NR==1 { 
-    maxlen_pid = length($1)
-    maxlen_user = length($2)
-    maxlen_state = length($3)
-    maxlen_pcpu = length($4)
-    maxlen_etime = length($5)
-}
+ps f -o pid,user,pcpu,state,etime,args -N --ppid 2 | awk '
 
-
+# Spool up output rows so they can be printed with proper width at the end
 {
+    # The whole command string is treated as one column even though it
+    # could contain spaces
     argv = $6
     for (i = 7; i <= NF; i++) {
         argv = argv " " $i
@@ -25,14 +19,30 @@ NR==1 {
 
     pids[NR] = $1
     users[NR] = $2
-    states[NR] = $3
-    pcpus[NR] = $4
+    pcpus[NR] = $3
+    states[NR] = $4
     etimes[NR] = $5
     argvs[NR] = argv
 }
 
 
+NR==1 { 
+    $3 = "CPU"  # replace "%CPU" with just "CPU"
+
+    pcpus[1] = "CPU"
+
+    maxlen_pid = length($1)
+    maxlen_user = length($2)
+    maxlen_pcpu = length($3)
+    maxlen_state = length($4)
+    maxlen_etime = length($5)
+}
+
+
 NR>1 {
+    
+    # Make etime more compact by converting days-hours:minutes:seconds
+    # to just hours:minutes:seconds
     etime = $5
 
     split(etime, days_hms, "-")
@@ -54,18 +64,21 @@ NR>1 {
         }
     }
 
+
+    # Update column widths
     if (length($1) > maxlen_pid) { maxlen_pid = length($1) }
     if (length($2) > maxlen_user) { maxlen_user = length($2) }
-    if (length($3) > maxlen_state) { maxlen_state = length($3) }
-    if (length($4) > maxlen_pcpu) { maxlen_pcpu = length($4) }
+    if (length($3) > maxlen_pcpu) { maxlen_pcpu = length($3) }
+    if (length($4) > maxlen_state) { maxlen_state = length($4) }
     if (length(etime) > maxlen_etime) { maxlen_etime = length(etime) }
 }
 
 END {
-    fmt = "%"maxlen_pid"s %"maxlen_user"s %"maxlen_state"s %"maxlen_pcpu"s  %"maxlen_etime"s %s\n"
+    # Print all rows with correct widths
+    fmt = "%"maxlen_pid"s %"maxlen_user"s %"maxlen_pcpu"s %-"maxlen_state"s %"maxlen_etime"s %s\n"
 
     for (i = 1; i <= NR; i++) {
-        printf fmt, pids[i], users[i], states[i], pcpus[i], etimes[i], argvs[i]
+        printf fmt, pids[i], users[i], pcpus[i], states[i], etimes[i], argvs[i]
     }
 }
 ' | awk "BEGIN { cols=$(tput cols) }     { print substr(\$0, 1, cols) }"
