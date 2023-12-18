cba4fa2808cb3bb8a4895074e3e6f0586319ead7
Adam Priebe <adam.priebe@broadcom.com>
2023-12-17 22:11:36 -0600

    fastprompt updates

--- fastprompt/prompt-client.zsh
+++ fastprompt/prompt-client.zsh
@@ -3,18 +3,35 @@
 # subprocesses)
 
 # TODO update this line with your installation path of fastprompt
-_FASTPROMPT_BASE="$HOME/projects/linux-utils/fastprompt"
+_FASTPROMPT_BASE="${HOME}/libexec"
+_FASTPROMPT_TMP="${XDG_RUNTIME_DIR:-/tmp}"
 
-_FASTPROMPT_FIFO_PATH="/tmp/fastprompt.$$.fifo"
+# Pidfiles and pipes: per-process
+_FASTPROMPT_FIFO="${_FASTPROMPT_TMP}/fastprompt.$$.ps1.fifo"
+_FASTPROMPT_FIFO_RPS1="${_FASTPROMPT_TMP}/fastprompt.$$.rps1.fifo"
+_FASTPROMPT_PIDFILE="${_FASTPROMPT_TMP}/fastprompt.$$.pid"
 
-rm -f "$_FASTPROMPT_FIFO_PATH"
-mkfifo "$_FASTPROMPT_FIFO_PATH"
+# Kill existing server when reloading
+if [ -f "$_FASTPROMPT_PIDFILE" ]; then
+    kill $(< "$_FASTPROMPT_PIDFILE") 2>/dev/null
+fi
+
+# Recreate fifos when reloading
+rm -f "$_FASTPROMPT_FIFO"
+mkfifo "$_FASTPROMPT_FIFO"
+
+rm -f "$_FASTPROMPT_FIFO_RPS1"
+mkfifo "$_FASTPROMPT_FIFO_RPS1"
 
 # Start the server process in the background, and wait for it to become ready.
 ready=0
 trap 'ready=1' SIGUSR1
-"$_FASTPROMPT_BASE"/prompt-server.py "$_FASTPROMPT_FIFO_PATH" &
+
+nohup "${_FASTPROMPT_BASE}/prompt-server.py" "$_FASTPROMPT_FIFO" "$_FASTPROMPT_FIFO_RPS1" >/dev/null 2>&1 &!
 serv_pid=$!
+
+echo "$serv_pid" >| "$_FASTPROMPT_PIDFILE"
+
 trap "kill $serv_pid" EXIT
 while [ $ready -ne 1 ]; do
     sleep 0.1
@@ -22,7 +39,11 @@ done
 
 # Generate the prompt via the precmd function, instead of setting PS1 statically.
 precmd() {
+    extra_precmd
+
     kill -SIGUSR1 $serv_pid
-    read _prompt < "$_FASTPROMPT_FIFO_PATH"
+    read _prompt < "$_FASTPROMPT_FIFO"
+    read _rprompt < "$_FASTPROMPT_FIFO_RPS1"
     PS1="$_prompt "
+    RPS1="$_rprompt"
 }
--- fastprompt/prompt-server.py
+++ fastprompt/prompt-server.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/local/bin/python3 -S
 
 import os
 import sys
@@ -6,24 +6,163 @@ import time
 import signal
 import socket
 from datetime import datetime
+import getpass
+import subprocess
+import fcntl
+import termios
+import struct
 
+
+# TODO change to your main username - when running as a different user, their username
+# will show up in the prompt. When running as the primary user, it's not shown
+# (so it doesn't take up space)
+PRIMARY_USER = getpass.getuser()
+
+# linux-utils cwd abbreviator
+CWDABBR_EXE = os.environ.get("CWDABBR_EXE", os.path.expanduser("~/libexec/cwdabbr"))
+
+STATE_UPDATE_INTERVAL_S = 1
+
+STTY_EXE = "/usr/bin/stty"
+GIT_EXE = "/usr/bin/git"
+
+if not sys.argv[2:]:
+    print(f"usage: {sys.argv[0]} fifo_ps1 fifo_rps1")
+    sys.exit(1)
 
 parent_pid = os.getppid()
+parent_pid_cwdfile = f"/proc/{parent_pid}/cwd"
+parent_tty = os.readlink(f"/proc/{parent_pid}/fd/0")
 
 hostname = socket.gethostname().split(".")[0]
-whoami = os.getlogin()
+whoami = getpass.getuser()
+
+# Prompt is of the form
+#   {prefix}{cwdabbr}{suffix}
+# Prefix and suffix won't change, so just calculate them once
+
+# Format 1
+# when user != primary user: [user@host cwd]$
+# when user == primary user: [host cwd]$
+if False:
+    prompt_prefix = "%f%k["
+    if whoami != PRIMARY_USER:
+        prompt_prefix += f"%F{{red}}%B{whoami}%f%B@"
+    else:
+        prompt_prefix += "%B"
+    prompt_prefix += f"{hostname}%b "
+    prompt_suffix = "%f]%(!.#.$)"
+
+# Format 2
+# when user != primary user: user@host[cwd]:
+# when user == primary user: host[cwd]:
+if True:
+    prompt_prefix = "%f%k"
+    if whoami != PRIMARY_USER:
+        prompt_prefix += f"%F{{red}}%B{whoami}%f%B@"
+    else:
+        prompt_prefix += "%B"
+    prompt_prefix += f"{hostname}%b["
+    prompt_suffix = "%f]:"
+
+
+class State:
+    term_cols = 0
+    git_branch = ""
+    datetime = ""
+    cwd = ""
+    cwd_abbr = ""
+
+STATE = State()
+
+def update_terminal_cols(pts):
+    try:
+        # ew. but it's the only thing I could get working.
+        # Probably because of how shell redirections work.
+        result = subprocess.run(f"{STTY_EXE} size < {pts}", shell=True, capture_output=True, text=True, check=True)
+        rows, cols = map(int, result.stdout.split())
+        STATE.term_cols = cols
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e}")
+        return 80
+
+def git_get_branch():
+    p = subprocess.Popen(
+            [GIT_EXE, "symbolic-ref", "-q", "--short", "HEAD"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            text=True
+    )
+    stdout, _ = p.communicate()
+    if p.returncode == 0:
+        STATE.git_branch = stdout.strip()
+        return
+
+    p = subprocess.Popen(
+            [GIT_EXE, "describe", "--tags", "--exact-match"],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.DEVNULL,
+            text=True
+    )
+    stdout, _ = p.communicate()
+    if p.returncode == 0:
+        STATE.git_branch = stdout.strip()
+    else:
+        STATE.git_branch = ""
+
+def update_datetime():
+    STATE.datetime = datetime.now().strftime("%m/%d %H:%M")
+
+def get_cwdabbr():
+    cwd = os.readlink(parent_pid_cwdfile)
+    if cwd != STATE.cwd:
+        os.chdir(cwd)
+        git_get_branch()
+        STATE.cwd = cwd
+        cols = STATE.term_cols
+        if cols < 145:
+            args = [CWDABBR_EXE, "--abbreviate", "--short"]
+        else:
+            args = [CWDABBR_EXE, "--abbreviate"]
+        p = subprocess.Popen(args, stdout=subprocess.PIPE, text=True)
+        stdout, _ = p.communicate()
+        cwd_abbr = stdout.replace("$", "\\\\\\$")
+        STATE.cwd_abbr = cwd_abbr
+        return cwd_abbr
+    else:
+        return STATE.cwd_abbr
+
+def update_state():
+    update_terminal_cols(parent_tty)
+    git_get_branch()
+    update_datetime()
 
 def write_prompt_to_fifo():
-    parent_cwd = os.readlink(f"/proc/{parent_pid}/cwd")
-    parent_cwd_base = os.path.basename(parent_cwd)
+    cwdabbr = get_cwdabbr()
+    prompt = f"{prompt_prefix}{cwdabbr}{prompt_suffix}"
+    with open(sys.argv[1], "w") as fifo_ps1:
+        fifo_ps1.write(prompt)
 
-    timestamp = datetime.now().strftime("%H:%M")
+    right_prompt = ""
+    if STATE.term_cols > 145:
+        if STATE.git_branch:
+            right_prompt += f"%F{{green}}[{STATE.git_branch}]%f "
+        right_prompt += STATE.datetime
 
-    with open(sys.argv[1], "w") as fifo:
-        fifo.write(f"{timestamp} [{whoami}@{hostname} {parent_cwd_base}]$ ")
+    with open(sys.argv[2], "w") as fifo_rps1:
+        fifo_rps1.write(right_prompt)
 
 signal.signal(signal.SIGUSR1, lambda *_: write_prompt_to_fifo())
+
+update_state()
 os.kill(parent_pid, signal.SIGUSR1)
 
 while True:
-    time.sleep(10)
+    try:
+        os.kill(parent_pid, 0)
+    except OSError:
+        print("-- Parent process died, exiting... ---")
+        break
+    update_state()
+    time.sleep(STATE_UPDATE_INTERVAL_S)
+
